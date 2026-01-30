78b2271e0dca3b758ab05a2178cbad1d510d25f0
nodchip <nodchip@gmail.com>
2026-01-30 10:16:43 +0900

    Revert "やねうら王純正の勝率モデルに変更した。"
    This reverts commit fa4b0ca10998fc5a94b13dbe1669372993b5435c.

--- model/lightning_module.py
+++ model/lightning_module.py
@@ -1,7 +1,6 @@
 import lightning as L
 import ranger21
 import torch
-import torch.nn.functional as F
 from torch import Tensor, nn
 
 from .config import LossParams, ModelConfig
@@ -82,27 +81,32 @@ class NNUE(L.LightningModule):
             * self.model.quantization.nnue2score
         )
 
-        # 600 is the kPonanzaConstant scaling factor needed to convert the training net output to a score.
-        # This needs to match the value used in the serializer
-        nnue2score = 600
-        scaling = nnue2score
+        p = self.loss_params
+        # convert the network and search scores to an estimate match result
+        # based on the win_rate_model, with scalings and offsets optimized
+        q = (scorenet - p.in_offset) / p.in_scaling
+        qm = (-scorenet - p.in_offset) / p.in_scaling
+        qf = 0.5 * (1.0 + q.sigmoid() - qm.sigmoid())
 
-        q = scorenet / scaling
+        s = (score - p.out_offset) / p.out_scaling
+        sm = (-score - p.out_offset) / p.out_scaling
+        pf = 0.5 * (1.0 + s.sigmoid() - sm.sigmoid())
+
+        # blend that eval based score with the actual game outcome
         t = outcome
-        p = (score / scaling).sigmoid()
-
-        epsilon = 1e-12
-        teacher_entropy = -(p * (p + epsilon).log() + (1.0 - p) * (1.0 - p + epsilon).log())
-        outcome_entropy = -(t * (t + epsilon).log() + (1.0 - t) * (1.0 - t + epsilon).log())
-        teacher_loss = -(p * F.logsigmoid(q) + (1.0 - p) * F.logsigmoid(-q))
-        outcome_loss = -(t * F.logsigmoid(q) + (1.0 - t) * F.logsigmoid(-q))
-        lambda_ = self.loss_params.start_lambda + (self.loss_params.end_lambda - self.loss_params.start_lambda) * (
+        actual_lambda = p.start_lambda + (p.end_lambda - p.start_lambda) * (
             self.current_epoch / self.max_epoch
         )
-        result  = lambda_ * teacher_loss    + (1.0 - lambda_) * outcome_loss
-        entropy = lambda_ * teacher_entropy + (1.0 - lambda_) * outcome_entropy
-        loss = result.mean() - entropy.mean()
+        pt = pf * actual_lambda + t * (1.0 - actual_lambda)
+
+        # use a MSE-like loss function
+        loss = torch.pow(torch.abs(pt - qf), p.pow_exp)
+        if p.qp_asymmetry != 0.0:
+            loss = loss * ((qf > pt) * p.qp_asymmetry + 1)
+        loss = loss.mean()
+
         self.log(loss_type, loss, prog_bar=True)
+
         return loss
 
     def training_step(self, batch, batch_idx):
